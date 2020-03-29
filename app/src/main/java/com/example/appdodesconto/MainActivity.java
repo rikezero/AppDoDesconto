bb473b10d91ece7f3116ec5270b81dcd92e12b9f
Rikezero <rikezero@gmail.com>
2020-03-29 15:31:12 -0300

    Commit com layout finalizado. Pronto para lógica.

---
+++ app/src/main/java/com/example/appdodesconto/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.appdodesconto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
